6c922a79444c5e845c79ead0b1c137480a91f3de
Tasty-Programmer <kimhunsup159@gmail.com>
2022-02-27 16:57:43 +0900

    [ API ] Implement Issue 이슈 구현중 값이 저장되지 않는 문제가 발생함

--- issue/views.py
+++ issue/views.py
@@ -1,6 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'issue.html')
+from issue.models import Issue
+
+def upload_Issue(request):
+    """
+    이슈 내용 저장 및 내용 및 제목 없을시 예외처리
+    """
+    if request.method == 'GET':
+        return render(request, 'issue.html')
+    elif request.method == 'POST':
+        subject = request.POST.get('issueTitle',None)
+        content = request.POST.get('issueContents',None)
+        issue = Issue(
+            subject = subject,
+            content = content
+        )
+        issue.save()
+        return render(request,'issue.html')
+
+
+
+
